1fc2395df42742f21f7b3a0004cd0af30f78e60c
soueiii <kumamon0136w@gmail.com>
2022-03-26 23:39:34 +0000

    注文機能追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,9 +5,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-   @order = Order.new
+   @order = Order.new(order_params)
    @order.customer_id = current_customer.id
    @order.save
+   redirect_to public_verification_path(@order)
   end
+
+  def verification
+   binding.pry
+  end
+
+  private
+   def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method)
+   end
 
 end
